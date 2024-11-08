cb36db6e1134e30e26a0ca83ad4ec09b6a74c9d5
Abdullah Mubashir <abdullahmubashir009@gmail.com>
2024-11-08 18:07:45 +0400

    Update Exercise 01 - Coding Is Cool.py
    Made the code more organized.

--- Exercise 01 - Coding Is Cool.py
+++ Exercise 01 - Coding Is Cool.py
@@ -1,8 +1,8 @@
-# We need to make variables in order to assign words
+# Step 1: We need to make variables in order to assign words
 
 word1 = "Coding"
 word2 = "IS"
 word3 = "Cool"
 
-# Use string concatenation to combine the variables and print the phrase
+# Step 2: Use string concatenation to combine the variables and print the phrase
 print(word1 + " " + word2 + " " + word3)
